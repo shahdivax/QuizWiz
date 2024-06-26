5845d4c832f35ba6491eadb2974e31b380d797f3
Divax <diya27divax1234@gmail.com>
2024-06-26 16:32:58 +0530

    New Website

--- custombot/static/js/main.js
+++ custombot/static/js/main.js
@@ -110,7 +110,7 @@ botForm.addEventListener('submit', async (e) => {
 
         const embedCode = `
 <div id="custombot-container"></div>
-<script src="https://cdn.jsdelivr.net/gh/shahdivax/QuizWiz/custombot/static/js/widget.js"><\/script>
+<script src="https://cdn.jsdelivr.net/gh/shahdivax/QuizWiz@master/custombot/static/js/widget.js"><\/script>
 <script>
     CustomBot.init({
         serverUrl: '${data.serverUrl}',
